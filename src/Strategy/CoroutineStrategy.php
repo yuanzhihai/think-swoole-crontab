1ce4b4144975e540264cc04b1d2e005bbf0b4af4
yuanzhihai <396751927@qq.com>
2023-06-15 13:48:25 +0800

    update

--- src/Strategy/CoroutineStrategy.php
+++ src/Strategy/CoroutineStrategy.php
@@ -5,19 +5,38 @@ namespace ThinkCrontab\Strategy;
 
 use Carbon\Carbon;
 use Swoole\Coroutine;
+use think\App;
 use ThinkCrontab\Crontab;
 
 class CoroutineStrategy extends AbstractStrategy
 {
+
+    protected Executor $executor;
+
+
+    public function __construct(App $app)
+    {
+        $this->executor = $app->make( Executor::class );
+        parent::__construct( $app );
+    }
+
     public function dispatch(Crontab $crontab)
     {
         Coroutine::create( function () use ($crontab) {
             if ($crontab->getExecuteTime() instanceof Carbon) {
                 $wait = $crontab->getExecuteTime()->getTimestamp() - time();
                 $wait > 0 && Coroutine::sleep( $wait );
-                $executor = $this->app->make( Executor::class );
-                $executor->execute( $crontab );
+                $this->executor->execute( $crontab );
             }
         } );
     }
+
+
+    public function executeOnce(Crontab $crontab)
+    {
+        Coroutine::create( function () use ($crontab) {
+            $this->executor->execute( $crontab );
+        } );
+    }
+
 }
